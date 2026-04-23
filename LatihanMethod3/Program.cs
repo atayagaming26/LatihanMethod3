8918b22d530878b87f2d8d4b265f6c0a05a80b75
atayagaming26 <atayagaming26@gmail.com>
2026-04-23 11:54:37 +0700

    LatihanMethod3

--- LatihanMethod3/Program.cs
+++ LatihanMethod3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahitem;
+decimal hargaItem, diskon, totalhargabayar;
+
+static decimal hitungDiskon(int jml_item)
+{
+    if (jml_item == 5) return 50000m;
+    else if (jml_item == 10) return 75000m;
+    else return 0;
+}
+
+static decimal hitungTotal(int jml_item, decimal hrg_item, decimal diskon_item)
+{
+    return jml_item * hrg_item - diskon_item;
+}
+
+Console.Write("Masukkan Jumlah Item: ");
+jumlahitem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukkan Harga Per Item: Rp ");
+hargaItem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahitem);
+
+totalhargabayar = hitungTotal(jumlahitem, hargaItem, diskon);
+
+Console.WriteLine($"Total yang harus dibayar adalah Rp {totalhargabayar}");
